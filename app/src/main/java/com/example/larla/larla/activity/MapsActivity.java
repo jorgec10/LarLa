9e105ea1e6f0c6313a30eba81e9c81bd2922dae9
jgallego <jorge.gallego1@um.es>
2018-04-20 20:18:28 +0200

    Maps added

--- app/src/main/java/com/example/larla/larla/activity/MapsActivity.java
+++ app/src/main/java/com/example/larla/larla/activity/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(38.023692, -1.174088);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Facultad de Informática"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
