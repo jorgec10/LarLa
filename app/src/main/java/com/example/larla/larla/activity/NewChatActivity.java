b2c4e619576f867b99749491c41dc2e2b984d2b5
jorgec10 <jorge.gallego.madrid@gmail.com>
2018-04-23 15:58:41 +0200

    New chat and room info activities added

---
+++ app/src/main/java/com/example/larla/larla/activity/NewChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.larla.larla.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.larla.larla.R;
+
+public class NewChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_chat);
+    }
+}
